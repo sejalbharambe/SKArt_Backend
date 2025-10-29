3a1104ad18f0d102b8a3d4a3d65fb5d3a89d410a
sejal <sejalbharambe2003@gmail.com>
2025-10-29 16:31:41 +0530

    added deployed url

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -23,7 +23,10 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // applies to all endpoints
-                        .allowedOrigins("http://localhost:5173") // your React app URL
+                        .allowedOrigins(
+                            "https://sk-art-frontend.vercel.app",
+                            "http://localhost:5173"//local url
+                            ) 
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
